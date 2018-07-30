65b9be2ee1f13326c10d4bc190cb08cc2deeaf78
Ambar de Alejandría <antestumejorsonrisa@gmail.com>
2018-07-30 12:59:00 -0500

    Se hace una copia de las imágenes seleccionadas para el collage, permitiéndo a su vez cambiar de imágen dentro del collage. Se agrega además el archivo 'DragDropTouch.js' que hace la función de un pollyfill

--- js/app.js
+++ js/app.js
@@ -1,26 +1,54 @@
+// let originalImage;
+let containerCollage =$(".available-to-drop");
 
-$('.for-collage').on('dragstart', (e) => {
-    e.originalEvent.dataTransfer.setData('key', e.target.id);
+$(".for-collage").on('dragstart', (e) => {
+    originalImage = e.originalEvent.dataTransfer.setData('key', e.target.id);
     // effectAllowed = 'move';
-    console.log("it's draggin...")
+    console.log("it's draggin...");
+    e.target.style.opacity = '0.4';
 })
  
-$('.available-to-drop').on('dragover', (e) => {
+containerCollage.on('dragover', (e) => {
     e.preventDefault();
-    console.log("it's dragover...")
+    console.log("it's dragover...");
+    console.log(e.currentTarget.childNodes.length);
+    if(e.currentTarget.childNodes.lenght == 1){
+        e.currentTarget.remove(1);
+    }
 })
 
-$('.available-to-drop').on('drop', (e) => {
+
+containerCollage.on('dragenter', (e) =>{
+    e.target.classList.add('over');
+})
+
+containerCollage.on('dragleave', (e) =>{
+    e.target.classList.remove('over');
+})
+
+containerCollage.on('drop', (e) => {
     let dropImage = e.originalEvent.dataTransfer.getData('key');
     e.preventDefault();
-    console.log("it's dropped...");
-    console.log(dropImage);
+    e.target.classList.add('fill-container');
+    e.target.classList.remove('over');
+    // console.log("it's dropped...");
+    // console.log(dropImage);
+    let currentImage = document.getElementById(dropImage);
 
-    e.target.appendChild(document.getElementById(dropImage));
+    let newImage = document.createElement('img');
+    newImage.classList.add('responsive-img');
+    newImage.src = currentImage.src;
+    
+    
+    console.log(e.currentTarget.childNodes.length);
 
-    // let newImage = $('.available-to-drop').add('img');
-    // newImage.src = dropImage.src;
-    // $('.available-to-drop').append(newImage);
-    // e.stopPropagation();
+     if(e.currentTarget.childNodes.length == 1){
+        e.currentTarget.removeChild(e.currentTarget.childNodes[0]);
+        e.currentTarget.appendChild(newImage);
+    
+    } else {
+        e.currentTarget.appendChild(newImage);
+    }
+  
 })
 
