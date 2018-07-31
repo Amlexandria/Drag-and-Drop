669b7adcef4fb243baf35d2efcdc83d0f1ca944c
Ambar de Alejandría <antestumejorsonrisa@gmail.com>
2018-07-30 20:55:50 -0500

    Se mejora el estilo visual de la página

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
-// let originalImage;
+'use strict';
+
+let image = $(".for-collage");
 let containerCollage =$(".available-to-drop");
 
-$(".for-collage").on('dragstart', (e) => {
-    originalImage = e.originalEvent.dataTransfer.setData('key', e.target.id);
-    // effectAllowed = 'move';
+//---- Se inicializan las imágenes para arrastrar y se define la data que se enviará al receptor (contenedor)
+image.on('dragstart', (e) => {
+    e.originalEvent.dataTransfer.setData('key', e.target.id);
     console.log("it's draggin...");
     e.target.style.opacity = '0.4';
 })
- 
-containerCollage.on('dragover', (e) => {
-    e.preventDefault();
-    console.log("it's dragover...");
-    console.log(e.currentTarget.childNodes.length);
-    if(e.currentTarget.childNodes.lenght == 1){
-        e.currentTarget.remove(1);
-    }
-})
 
+// ---- Se preparan los contenedores para recibir las imágenes
+containerCollage.on('dragover', (e) => {
+    e.preventDefault();//para lo que hay que prevenir "remover" el default (no permitir que entre otro elemento)
+    console.log("it's dragover...");
+    // console.log(e.currentTarget.childNodes.length);
+    
+})
 
 containerCollage.on('dragenter', (e) =>{
     e.target.classList.add('over');
@@ -26,21 +26,20 @@ containerCollage.on('dragleave', (e) =>{
     e.target.classList.remove('over');
 })
 
+// ---- Se obtiene la data (imágenes) y se "reciben" en los contendores definidos
 containerCollage.on('drop', (e) => {
     let dropImage = e.originalEvent.dataTransfer.getData('key');
     e.preventDefault();
-    e.target.classList.add('fill-container');
     e.target.classList.remove('over');
-    // console.log("it's dropped...");
-    // console.log(dropImage);
+    console.log("it's dropped...");
     let currentImage = document.getElementById(dropImage);
 
     let newImage = document.createElement('img');
-    newImage.classList.add('responsive-img');
+    newImage.classList.add("responsive-img", "circle");
     newImage.src = currentImage.src;
     
     
-    console.log(e.currentTarget.childNodes.length);
+    // console.log(e.currentTarget.childNodes.length);
 
      if(e.currentTarget.childNodes.length == 1){
         e.currentTarget.removeChild(e.currentTarget.childNodes[0]);
@@ -51,4 +50,9 @@ containerCollage.on('drop', (e) => {
     }
   
 })
+
+image.on('dragend', (e) =>{
+    e.target.style.opacity = '1';
+})
+
 
